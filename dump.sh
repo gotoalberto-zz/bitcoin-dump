4d0136c50fd97c5f7b2835a5f7614725e8232a9b
Alberto Gomez Toribio <agtoribio@paradigmatecnologico.com>
2013-08-20 16:32:43 +0200

    commit
    commit

--- dump.sh
+++ dump.sh
@@ -1,24 +1,22 @@
 #!/bin/bash
 
+mkdir data
 #############################################################################
 ########################## CONFIG
 #############################################################################
 JSAWKPATH="/users/gotoalberto/jsawk/jsawk"
-RPCMINNERIP="127.0.0.1"
-RPCMINNERPORT="8332"
-RPCMINNERUSER="bitcoin"
-RPCMINNERPASSWORD="password"
+RPCMINNERIP="rpc.blockchain.info"
+RPCMINNERPORT="443"
+RPCMINNERUSER="c49c5c4d-1b4c-488d-8a7b-065d0a96310a"
+RPCMINNERPASSWORD="bitcointest"
 
 #############################################################################
-########################## Obtain downloaded blocks number
+########################## Obtain blocks number on blockchain
 #############################################################################
-CMD="curl --data-binary '{\"jsonrpc\": \"1.0\", \"id\":\"curltest\", \"method\": \"getinfo\", \"params\": [] }'  -H 'content-type: text/plain;' http://$RPCMINNERUSER:$RPCMINNERPASSWORD@$RPCMINNERIP:$RPCMINNERPORT | $JSAWKPATH 'return this.result.blocks'"
-BLOCKS=$()
-rm blocksnumber.txt
-echo "$BLOCKS" > blocksnumber.txt
-
-BLOCKS=($(<blocksnumber.txt))
-
+CMD="curl --data-binary '{\"jsonrpc\": \"2.0\", \"id\":\"bitcoin\", \"method\": \"getinfo\", \"params\": [] }'  -H 'content-type: text/plain;' https://$RPCMINNERUSER:$RPCMINNERPASSWORD@$RPCMINNERIP:$RPCMINNERPORT |  sed -E 's|([a-z0-9]{64})|"\1"|g' | $JSAWKPATH 'return this.result.blocks'"
+BLOCKS=$(eval $CMD)
+echo "ACTUALLY THERE ARE $BLOCKS BLOCKS ON BLOCKCHAIN.INFO"
+((BLOCKS--))
 #Set current processed block
 
 if [ -f 'currentblock.txt' ];
@@ -26,52 +24,80 @@ then
    echo "The execution continues on BLOCK $(<currentblock.txt)"
 else
    echo "Starting dump on BLOCK 1"
-   echo 1 >> currentblock.txt
+   echo "$BLOCKS" > currentblock.txt
 fi
 CURRENTBLOCK=($(<currentblock.txt))
 
 #############################################################################
 ########################## Extract data
 #############################################################################
+echo "$BLOCKS"
 
-while [ $CURRENTBLOCK -le $BLOCKS ]
+while [ $CURRENTBLOCK -gt 1 ]
 do
+	#Obtain hash of current block to process
 	echo "PROCESSING BLOCK $CURRENTBLOCK FROM $BLOCKS"
-	CMD="curl --data-binary '{\"jsonrpc\": \"1.0\", \"id\":\"bitcoin\", \"method\": \"getblockhash\", \"params\": [$CURRENTBLOCK] }'  -H 'content-type: text/plain;' http://$RPCMINNERUSER:$RPCMINNERPASSWORD@$RPCMINNERIP:$RPCMINNERPORT | $JSAWKPATH 'return this.result'"
-    CURRENTHASH=$(eval $CMD)
+	CMD="curl --data-binary '{\"jsonrpc\": \"2.0\", \"id\":\"bitcoin\", \"method\": \"getblockhash\", \"params\": [$CURRENTBLOCK] }'  -H 'content-type: text/plain;' https://$RPCMINNERUSER:$RPCMINNERPASSWORD@$RPCMINNERIP:$RPCMINNERPORT | $JSAWKPATH 'return this.result'"
+	CURRENTHASH=$(eval $CMD)
 
-	CMD="curl --data-binary '{\"jsonrpc\": \"1.0\", \"id\":\"bitcoin\", \"method\": \"getblock\", \"params\": [\"$CURRENTHASH\"] }'  -H 'content-type: text/plain;' http://$RPCMINNERUSER:$RPCMINNERPASSWORD@$RPCMINNERIP:$RPCMINNERPORT | $JSAWKPATH 'return this.join(\"\\n\")' 'return this.result.tx.join(\"\\n\")' "
-	TX=$(eval $CMD)
+	#Save all transactions to txtemp.txt
+	CMD="curl http://blockexplorer.com/rawblock/$CURRENTHASH | $JSAWKPATH 'forEach(this.tx, \"out(item)\").join(\"\\n\")'"
+	TXS=$(eval $CMD)
 	rm txtemp.txt
-	echo "$TX" > txtemp.txt
+	rm richtxtemp.txt
+	echo "$TXS" > txtemp.txt
 
-	for txid in $(cat txtemp.txt)
+	LINE="1"
+	#Add IP and GEO info
+	for tx in $(cat txtemp.txt)
 	do
-		#obtain transaction ip 
-		CMD="curl http://blockchain.info/es/tx/$txid)"
-		BCPAGE=$(eval $CMD)
-		rm blockchainpage.txt
-		echo "$BCPAGE" > blockchainpage.txt
-
-		CMD="cat blockchainpage.txt | grep \"ip-address/\" | grep -Eo '([0-9]{1,3}\.){3}[0-9]{1,3}' | uniq"
-		IP=$(eval $CMD)
-		echo "$IP"
-
-		#obtain country from ip
-		CMD="curl ipinfo.io/$IP | /users/gotoalberto/jsawk/jsawk 'return this.country'"
-		COUNTRY=$(eval $CMD)
-
-		#obtain tx data
-		CMD="curl --data-binary '{\"jsonrpc\": \"1.0\", \"id\":\"bitcoin\", \"method\": \"getrawtransaction\", \"params\": [\"$txid\"] }'  -H 'content-type: text/plain;' http://$RPCMINNERUSER:$RPCMINNERPASSWORD@$RPCMINNERIP:$RPCMINNERPORT | $JSAWKPATH 'return this.result'"
-		RAWTX=$(eval $CMD)
-
-		if [ "$RAWTX" == "" ]
+		CMD="cat txtemp.txt | wc -l | tr -d ' '"
+		COUNT=$(eval $CMD)
+		if [ "$LINE" -le "$COUNT" ]
 		then
-			echo "$RAWTX" >> txfails.txt
-		else
-			CMD="curl --data-binary '{\"jsonrpc\": \"1.0\", \"id\":\"bitcoin\", \"method\": \"decoderawtransaction\", \"params\": [\"$RAWTX\"] }'  -H 'content-type: text/plain;' http://$RPCMINNERUSER:$RPCMINNERPASSWORD@$RPCMINNERIP:$RPCMINNERPORT | $JSAWKPATH 'return this.result'"
-			DECODEDTX=$(eval $CMD)
-			echo "$DECODEDTX" >> transactions.tx
-		fi		
+			CMD="sed '$LINE q;d' txtemp.txt | $JSAWKPATH 'return this.hash'"
+			TXID=$(eval $CMD)
+
+			#get transaction ip 
+			CMD="curl http://blockchain.info/es/tx/$TXID"
+			BCPAGE=$(eval $CMD)
+			rm blockchainpage.html
+			echo "$BCPAGE" > blockchainpage.html
+
+			CMD="cat blockchainpage.html | grep \"ip-address/\" | grep -Eo '([0-9]{1,3}\.){3}[0-9]{1,3}' | uniq"
+			IP=$(eval $CMD)
+			
+			#get country by ip if ip is not null
+			COUNTRY=""
+			if [ "$IP" != "" ]
+			then
+				CMD="curl ipinfo.io/$IP | $JSAWKPATH 'return this.country'"
+				COUNTRY=$(eval $CMD)
+			fi
+
+			#add ip and geo info to transaction json as nodes
+			CMD="sed '$LINE q;d' txtemp.txt | sed 's/\"ver\"/\"geo\":\"$COUNTRY\",\"ip\":\"$IP\",\"ver\"/g'"
+			RICHTX=$(eval $CMD)
+			echo "$RICHTX" >> richtxtemp.txt
+			((LINE++))
+		fi
 	done
+
+	#Add transactions to final file
+	CMD="cat richtxtemp.txt >> data/transactions.txt"
+	eval $CMD
+
+	#Split file if have more than x lines
+	CMD="cat dump.sh | wc -l | tr -d ' '"
+	LINESCOUNT=$(eval $CMD)
+
+	if [ $LINESCOUNT -gt 5000 ]
+	then
+		TIMESTAMP=$(date +%m%d%y%H%M%S)
+		CMD="mv data/transactions.tx data/transactions$TIMESTAMP.txt"
+		eval $CMD
+	fi
+
+	((CURRENTBLOCK--))
+	echo "$CURRENTBLOCK" > currentblock.txt
 done
